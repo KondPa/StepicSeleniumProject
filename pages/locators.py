c0ae599d50920093e9243b4cdb5bd161e4eea739
KondPa <rangig@yandex.ru>
2019-10-03 15:52:44 +0300

    Added new Object Page and series of tests for it

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     LOGIN_EMAIL_FIELD = (By.CSS_SELECTOR, "#id_login-username")
@@ -15,3 +16,17 @@ class LoginPageLocators:
     REGISTRATION_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password1")
     REGISTRATION_PASSWORD_CONFIRM_FIELD = (By.CSS_SELECTOR, "#id_registration-password2")
     REGISTRATION_BUTTON = (By.NAME, "registration_submit")
+
+
+class ProductPageLocators:
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "#content_inner .product_main h1")
+    PRODUCT_ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT_ADD_TO_WISHLIST_BUTTON = (By.CSS_SELECTOR, "button.btn-wishlist")
+    PRODUCT_WRITE_REVIEW_BUTTON = (By.CSS_SELECTOR, "#write_review")
+    PRODUCT_HEADER_CART_INFO = (By.CSS_SELECTOR, ".basket-mini")
+    PRODUCT_BASKET_ALERT = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_HEADER_GO_TO_BASKET = (By.CSS_SELECTOR, ".btn-group > a")
+    PRODUCT_ALERT_NAME_OF_ADDED_GOOD = (By.CSS_SELECTOR, "div.alert:nth-child(1) strong")
+    PRODUCT_ALERT_GO_TO_BASKET = (By.CSS_SELECTOR, ".alertinner a.btn-info:nth-child(1)")
+    PRODUCT_ALERT_GO_TO_CHECKOUT = (By.CSS_SELECTOR, ".alertinner a.btn-info:nth-child(2)")
---
+++ pages/product_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def should_be_product(self):
+        assert self.is_element_present(
+            *ProductPageLocators.PRODUCT_ADD_TO_CART_BUTTON), "Product add to cart button is missing"
+
+    def product_add_to_cart(self):
+        button = self.browser.find_element(*ProductPageLocators.PRODUCT_ADD_TO_CART_BUTTON)
+        button.click()
+
+    def product_should_be_added_to_cart(self):
+        number = self.browser.find_element(*ProductPageLocators.PRODUCT_BASKET_ALERT).text
+        price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        added_name = self.browser.find_element(*ProductPageLocators.PRODUCT_ALERT_NAME_OF_ADDED_GOOD).text
+        assert str(name) == str(added_name), f"Name '{name}' should be equal to '{added_name}'"
+        assert number == price, f"Price '{price}' should be equal to '{number}'"
